9103beda7ec5be46872f1c880a219f43e45635d9
sunarta <71360199+gurubaru@users.noreply.github.com>
2026-05-08 13:22:06 +0800

    Improve lazy loading by preloading images

--- script.js
+++ script.js
@@ -324,17 +324,38 @@ if(parallaxDoor){
     const lazyImages = document.querySelectorAll('img[data-src]');
 
     const lazyObserver = new IntersectionObserver((entries, observer)=>{
+
         entries.forEach(entry=>{
+
             if(entry.isIntersecting){
+
                 const img = entry.target;
-                img.src = img.dataset.src;
+
+                const newImg = new Image();
+
+                newImg.src = img.dataset.src;
+
+                newImg.onload = ()=>{
+
+                    img.src = newImg.src;
+
+                    requestAnimationFrame(()=>{
+
+                        img.classList.add('loaded');
+
+                    });
+
+                };
+
                 img.removeAttribute('data-src');
+
                 observer.unobserve(img);
             }
+
         });
 
     },{
-        rootMargin: "0px 0px 200px 0px"
+        rootMargin:"0px 0px 300px 0px"
     });
 
     lazyImages.forEach(img=>lazyObserver.observe(img));
